ea5ad443fcdd36552f2d16110e250636fce69ac1
ANUAR_ARSTANBEKOV <kaliroseswiththegoldengun@gmail.com>
2022-06-28 01:37:27 +0600

    hover-change-img-animation

---
+++ app.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        img: "./assets/payment-bankCard.png"
+    },
+    {
+        img: "./assets/payment-bluetooth.png"
+    },
+    {
+        img: "./assets/payment-card.png"
+    },
+    {
+        img: "./assets/payment-nfc.png"
+    },
+    {
+        img: "./assets/payment-plateNumber.png"
+    },
+    {
+        img: "./assets/payment-qr.png"
+    },
+    {
+        img: "./assets/payment-sms.png"
+    },
+];
+
