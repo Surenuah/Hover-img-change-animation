8c58197f7c281c5a53aa7e18aa616b557cc2afed
ANUAR_ARSTANBEKOV <kaliroseswiththegoldengun@gmail.com>
2022-06-28 01:42:35 +0600

    hover change animation

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 const products = [
     {
-        img: "./assets/payment-bankCard.png"
-    },
-    {
-        img: "./assets/payment-bluetooth.png"
-    },
-    {
-        img: "./assets/payment-card.png"
-    },
-    {
-        img: "./assets/payment-nfc.png"
+        img: "./assets/payment-qr.png"
     },
     {
         img: "./assets/payment-plateNumber.png"
     },
     {
-        img: "./assets/payment-qr.png"
+        img: "./assets/payment-card.png"
+    },
+    {
+        img: "./assets/payment-bluetooth.png"
+    },
+    {
+        img: "./assets/payment-bankCard.png"
     },
     {
         img: "./assets/payment-sms.png"
     },
+    {
+        img: "./assets/payment-nfc.png"
+    },
 ];
 
+let chosenProduct = products[0];
+const paymentMethodsItem =document.querySelectorAll(".payment-methods__item");
+const currentProductImg = document.querySelector(".payment-methods__img");
+
+paymentMethodsItem.forEach((item, index) => {
+    // changing image
+    item.addEventListener("mouseenter", () => {
+        chosenProduct = products[index];
+        currentProductImg.src = chosenProduct.img;
+    });
+});
